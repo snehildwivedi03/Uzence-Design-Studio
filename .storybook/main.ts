00e29e46deefd2815bced751a4c5583aee6f14d3
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-09-10 21:22:05 +0530

    Project Completed

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,27 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: async (viteConfig) => {
+    // ✅ Fix crypto issue in Node 21+
+    viteConfig.define = {
+      ...viteConfig.define,
+      global: {}, // fallback for some libs expecting global
+    };
+
+    return viteConfig;
+  },
 };
+
 export default config;
